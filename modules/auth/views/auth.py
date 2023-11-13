e79ca5913f05176c9d621f692ac02aa28d964939
dreamlight7274 <dreamlight7274@gmail.com>
2023-11-13 09:16:13 +0000

    base
    base part

--- modules/auth/views/auth.py
+++ modules/auth/views/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 path_auth = Blueprint('auth', __name__, url_prefix='/auth',
                       static_folder='../static', template_folder='../templates')
 @path_auth.route('/login', methods=['GET', 'POST']) # method support
 def login():
+    if request.method == 'POST':
+            print(request.form.get('username'))
     return render_template('login.html')
 
 @path_auth.route('/register', methods=['GET', 'POST']) # method support
