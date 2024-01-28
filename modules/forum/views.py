70f6c99f1bd4615f2e64540015cd6d4c543ed182
dreamlight7274 <dreamlight7274@gmail.com>
2024-01-28 21:29:48 +0000

    personal_showing
    personal showing

--- modules/forum/views.py
+++ modules/forum/views.py
@@ -9,7 +9,7 @@ def index():
     #if you want to have a test, use this: articles_showing = [1, 2, 3]
 
     page = request.args.get('page', 1, type=int)
-    pagination = Article.query.order_by(Article.article_id).paginate(page=page, per_page=1, error_out=False)
+    pagination = Article.query.order_by(Article.article_id).paginate(page=page, per_page=10, error_out=False)
     articles_showing = pagination.items
     return render_template('index.html', articles = articles_showing, pagination=pagination)
 # @path_forum.route('/')
