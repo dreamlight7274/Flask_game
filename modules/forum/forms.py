f92b71abb52b7ab7cb6ddab0c9f3cb1169ec11e2
dreamlight7274 <dreamlight7274@gmail.com>
2024-01-31 23:25:50 +0000

    next level
    next level

--- modules/forum/forms.py
+++ modules/forum/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, SelectField, TextAreaField, SelectMultipleField, PasswordField, BooleanField
 from flask_wtf.file import FileField, file_size, file_allowed
 from wtforms.validators import DataRequired, Length
+from ..forum.models import ArticleStatus
 
 class Personal_info_edit_form(FlaskForm):
     username = StringField('username', validators=[
@@ -12,3 +13,37 @@ class Personal_info_edit_form(FlaskForm):
         file_allowed(['jpg','jpeg','png'], message="only jpg/jpeg/png file are available"),
         file_size(max_size=10240000, message="The size of the portrait should be less than 10 MB")
     ], description="the size of the portrait should be less than 10 MB, and jpg/jpeg/png files are accessible ")
+
+class Change_password_form(FlaskForm):
+    password = PasswordField('password', validators=[
+        Length(min=10, max=30, message="password should be less than 30 words and more than 10 words")
+
+    ], description="It's the password")
+    password_confirm = PasswordField('password_confirm', validators=[
+        Length(min=10, max=30, message="please input the password again")
+
+    ], description="This is for insurance")
+
+class Article_form_user(FlaskForm):
+    name = StringField('name', validators=[
+        DataRequired(message="please input the title"),
+        Length(max=30, message="the length of the name of article should less than 30 words")
+    ])
+    excerpt = StringField('excerpt', validators=[
+        DataRequired(message="please input the excerpt of the article"),
+        Length(max=100, message="The length of the excerpt of article should less than 100 words")
+    ])
+    status = RadioField('status',
+                        choices=(ArticleStatus.draft.name, ArticleStatus.online.name),
+                        default=ArticleStatus.online.name)
+    category = SelectField('category', choices=None, coerce=int, validators=[
+        DataRequired(message="please input the category"),
+    ])
+    content = TextAreaField('content', validators=[
+        DataRequired(message="Please input the content of article")
+    ])
+    thumbnail = FileField("thumbnail", validators=[
+        file_allowed(['jpg','jpeg','png'], message="only jpg/jpeg/png file are available"),
+        file_size(max_size=10240000, message="The size of the thumbnail should be less than 10 MB")
+    ], description="the size of the thumbnail should be less than 10 MB, and jpg/jpeg/png files are accessible ")
+    classifications = SelectMultipleField('classifications', choices=None, coerce=int)
--- modules/forum/views.py
+++ modules/forum/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, g
-from .models import Article, Category
+from werkzeug.security import generate_password_hash
+from .models import Article, Category, Classification
 from ..auth.models import User
 from ..auth.views.auth import login_request_update
-from ..forum.forms import Personal_info_edit_form
+from ..forum.forms import Personal_info_edit_form, Change_password_form, Article_form_user
 from ..admin.utils import upload_file
 from Project_public import db
 # print(__name__)
@@ -49,6 +50,119 @@ def personal_info_update(user_id):
         return redirect(url_for('forum.personal_index'))
     return render_template('forum/edit_personal_information.html', form=form_using, user=user_using, key="edit")
 
+@path_forum.route('/personal/password/change', methods=['GET','POST'])
+@login_request_update
+def personal_change_password():
+    user_using = User.query.get(g.user.user_id)
+    form_using = Change_password_form()
+    if form_using.validate_on_submit():
+        if form_using.password.data == form_using.password_confirm.data:
+            print('same')
+            user_using.password = generate_password_hash(form_using.password.data)
+            db.session.add(user_using)
+            db.session.commit()
+            g.user = user_using
+            flash('the password has been changed successfully', 'success')
+            return redirect(url_for('forum.personal_change_password'))
+        else:
+            print('different')
+            flash('the two passwords are different', 'fail')
+            return redirect(url_for('forum.personal_change_password'))
+    return render_template('forum/personal_change_password.html', form=form_using)
+
+@path_forum.route('/personal/articles')
+@login_request_update
+def personal_article_manage_page():
+    page = request.args.get('page', 1, type=int)
+    pagination = Article.query.filter_by(user_id=g.user.user_id).order_by(Article.article_id).paginate(page=page, per_page=2, error_out=False)
+    articles_show = pagination.items
+    return render_template('forum/personal_articles.html', articles=articles_show, pagination=pagination)
+
+@path_forum.route('/personal/article/add', methods=['GET','POST'])
+@login_request_update
+def personal_add_new_article():
+    form_using = Article_form_user()
+    form_using.category.choices = [(category.category_id, category.category_name) for category in Category.query.all()]
+    form_using.classifications.choices = [(cla.cla_id, cla.cla_name) for cla in Classification.query.all()]
+
+
+    if form_using.validate_on_submit():
+        file = form_using.thumbnail.data
+        article_creating = Article(
+            article_name=form_using.name.data,
+            excerpt=form_using.excerpt.data,
+            article_status=form_using.status.data,
+            category_id=int(form_using.category.data),
+            content=form_using.content.data,
+            user_id=g.user.user_id
+
+
+        )
+        if file == None:
+            article_creating.thumbnail = None
+        else:
+            thumbnail_path, thumbnail_name = upload_file('thumbnail', file)
+            file.save(thumbnail_path)
+            article_creating.thumbnail = f'thumbnail/{thumbnail_name}'
+        article_creating.classifications = [Classification.query.get(cla_id) for cla_id in form_using.classifications.data]
+        db.session.add(article_creating)
+        db.session.commit()
+        flash(f'Article {form_using.name.data} has been created')
+        return redirect(url_for('forum.personal_article_manage_page'))
+    return render_template('forum/personal_add_or_edit_article.html',form= form_using, key="create")
+
+@path_forum.route('/personal/article/edit/<int:article_id>', methods=['GET','POST'])
+@login_request_update
+def personal_edit_article(article_id):
+    article_using = Article.query.get(article_id)
+    classifications_using = [cla.cla_id for cla in article_using.classifications]
+    form_using = Article_form_user(
+        name=article_using.article_name,
+        excerpt=article_using.excerpt,
+        status=article_using.article_status,
+        category=article_using.category.category_id,
+        content=article_using.content,
+        thumbnail=article_using.thumbnail,
+        classifications=classifications_using,
+        user_id=g.user.user_id,
+
+
+    )
+
+    form_using.category.choices = [(category.category_id, category.category_name) for category in Category.query.all()]
+    form_using.classifications.choices = [(cla.cla_id, cla.cla_name) for cla in Classification.query.all()]
+    if form_using.validate_on_submit():
+        article_using.article_name = form_using.name.data
+        article_using.excerpt = form_using.excerpt.data
+        article_using.article_status = form_using.status.data
+        article_using.category_id = int(form_using.category.data)
+        article_using.content = form_using.content.data
+        file_update = form_using.thumbnail.data
+        if article_using.thumbnail == file_update:
+            article_using.thumbnail = article_using.thumbnail
+        else:
+            thumbnail_path, thumbnail_name = upload_file('thumbnail', file_update)
+            file_update.save(thumbnail_path)
+            article_using.thumbnail = f'thumbnail/{thumbnail_name}'
+        article_using.user_id = g.user.user_id
+        article_using.classifications = [Classification.query.get(cla_id) for cla_id in form_using.classifications.data]
+        db.session.add(article_using)
+        db.session.commit()
+        flash(f'Article {form_using.name.data} has been edited')
+        return redirect(url_for('forum.personal_article_manage_page'))
+
+
+    return render_template('forum/personal_add_or_edit_article.html', form=form_using, article=article_using, key="edit")
+
+@path_forum.route('/personal/article/delete/<int:article_id>', methods=['GET','POST'])
+@login_request_update
+def personal_delete_article(article_id):
+    article_using = Article.query.get(article_id)
+    if article_using:
+        db.session.delete(article_using)
+        db.session.commit()
+        flash(f'the article {article_using.article_name} has been deleted')
+        return redirect(url_for('forum.personal_article_manage_page'))
 
 
 @path_forum.route('/category/<int:cat_id>')
@@ -78,7 +192,6 @@ def search():
     pagination = Article.query.filter(Article.article_name.like("%"+words+"%")).paginate(page=page, per_page=2, error_out=False)
     articles_using = pagination.items
     return render_template('forum/index_with_search.html', words=words, articles=articles_using, pagination=pagination)
-
 
 
 
